f80fc9e582f621b7e20b5e3e5bfb5fb6435fb0be
Jerry Velazquez <jvr572@gmail.com>
2019-04-09 22:11:03 -0400

    Done.

--- app/models/car_owner.rb
+++ app/models/car_owner.rb
@@ -2,9 +2,27 @@ class CarOwner
 
   attr_reader :name
 
+  @@all = []
+
   def initialize(name)
     @name = name
+    @@all << self
   end
 
+  def self.all
+    @@all
+  end
+
+  def owns
+    Car.all.select {|own| own.car_owner == self}
+  end
+
+  def go_to_fixer
+    self.owns.map(&:mechanic)
+  end
+
+  def average_amount_cars
+    self.owns.size%Car.all.size
+  end
 
 end
